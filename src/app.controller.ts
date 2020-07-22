48aa0a47abb3a33122af85057b6643e5426f523a
Abed Zantout <abed@techhive.io>
2020-07-22 11:56:47 +0300

    feat(app): added jwt strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,39 @@
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 
-import { AppService } from './app.service';
 import { GateGuard } from '@th/permissions';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @GateGuard('moderator')
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private authService: AuthService) {
   }
 
-  @UseGuards(AuthGuard('local'))
+  // @UseGuards(AuthGuard('local'))
+  // @Get()
+  // @GateGuard('moderator')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  //
+
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req): Promise<any> {
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @GateGuard('moderator')
+  getModeratorProfile(@Request() req) {
     return req.user;
   }
 }
